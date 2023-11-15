c8ad4ad11309236a87907f25ceec437e55448147
GTPSHAX <143262216+GTPSHAX@users.noreply.github.com>
2023-11-16 05:58:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-window.addEventListener('load', function() {
-    const loadingElement = document.querySelector('.loading');
-    if (loadingElement) {
-      // Mengatur timeout untuk menghapus kelas "muncul" setelah beberapa waktu
-      setTimeout(function() {
-        loadingElement.classList.remove('muncul');
-      }, 2000); // Ganti nilai 2000 dengan durasi waktu yang diinginkan (dalam milidetik)
-    }
-  });
+let timeout;
 
-// Fungsi untuk mengecek apakah elemen dalam viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+const isElementInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const handleViewportChange = () => {
+  if (timeout) {
+    clearTimeout(timeout);
   }
-  
-  // Fungsi untuk menambah atau menghapus kelas "pop", "popo", dan "sudah-masuk" pada elemen
-  function handleScroll() {
+
+  timeout = setTimeout(() => {
     const items = document.querySelectorAll('.item');
-    
+  
     items.forEach(item => {
-      
-      if (isInViewport(item)) {
+      if (isElementInViewport(item)) {
         item.classList.add('pop');
         item.classList.remove('sudah-masuk');
       } else {
@@ -33,11 +27,9 @@ function isInViewport(element) {
         item.classList.add('sudah-masuk');
       }
     });
-  }
-  
-  // Mendengarkan event scroll
-  window.addEventListener('scroll', handleScroll);
-  
-  // Memanggil fungsi saat halaman dimuat untuk menentukan kelas awal
-  handleScroll();
-  
+  }, 100); // Ganti angka ini sesuai dengan kebutuhan untuk menentukan penundaan setelah scroll berhenti
+};
+
+window.addEventListener('scroll', handleViewportChange);
+window.addEventListener('resize', handleViewportChange);
+document.addEventListener('DOMContentLoaded', handleViewportChange);
