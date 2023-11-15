66ed000ce442a3d4433ac928e40b7b2622938acc
GTPSHAX <143262216+GTPSHAX@users.noreply.github.com>
2023-11-16 06:00:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-let timeout;
+window.addEventListener('load', function() {
+    const loadingElement = document.querySelector('.loading');
+    if (loadingElement) {
+      // Mengatur timeout untuk menghapus kelas "muncul" setelah beberapa waktu
+      setTimeout(function() {
+        loadingElement.classList.remove('muncul');
+      }, 2000); // Ganti nilai 2000 dengan durasi waktu yang diinginkan (dalam milidetik)
+    }
+  });
 
-const isElementInViewport = (el) => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
+  let timeout;
 
-const handleViewportChange = () => {
-  if (timeout) {
-    clearTimeout(timeout);
-  }
-
-  timeout = setTimeout(() => {
-    const items = document.querySelectorAll('.item');
+  const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
   
-    items.forEach(item => {
-      if (isElementInViewport(item)) {
-        item.classList.add('pop');
-        item.classList.remove('sudah-masuk');
-      } else {
-        item.classList.remove('pop');
-        item.classList.add('sudah-masuk');
-      }
-    });
-  }, 100); // Ganti angka ini sesuai dengan kebutuhan untuk menentukan penundaan setelah scroll berhenti
-};
-
-window.addEventListener('scroll', handleViewportChange);
-window.addEventListener('resize', handleViewportChange);
-document.addEventListener('DOMContentLoaded', handleViewportChange);
+  const handleViewportChange = () => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+  
+    timeout = setTimeout(() => {
+      const items = document.querySelectorAll('.item');
+    
+      items.forEach(item => {
+        if (isElementInViewport(item)) {
+          item.classList.add('pop');
+          item.classList.remove('sudah-masuk');
+        } else {
+          item.classList.remove('pop');
+          item.classList.add('sudah-masuk');
+        }
+      });
+    }, 100); // Ganti angka ini sesuai dengan kebutuhan untuk menentukan penundaan setelah scroll berhenti
+  };
+  
+  window.addEventListener('scroll', handleViewportChange);
+  window.addEventListener('resize', handleViewportChange);
+  document.addEventListener('DOMContentLoaded', handleViewportChange);
+  
